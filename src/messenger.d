45d614786d26937637fc9375a3b66656a4cad8d1
Student <student@itk-d650>
2017-02-02 14:44:29 +0100

    made structs, deleted swapfiles

---
+++ src/messenger.d
@@ -0,0 +1,94 @@
+/*
+imports
+
+define delegateOrder struct:
+	{
+		target ID : int
+		order	  : string
+	}
+	
+	
+define confirmOrder struct:
+	{
+		sender ID : int
+		order	  : string
+	}
+
+define expediteOrder struct:
+	{
+		sender ID : int
+		order	  : string
+	}
+
+define syncRequest struct:
+	{
+		sender ID : int
+	}
+
+define heartBeat Struct:
+	{
+		sender ID : int
+		sender State : state_t
+		sender Floor : int
+	}
+
+*/
+
+struct delegateOrder
+{
+	int targetID;
+	string order;
+}
+
+struct confirmOrder
+{
+	int senderID;
+	string order;
+}
+
+struct expediteOrder
+{
+	int senderID;
+	string order;
+}
+
+struct syncRequest
+{
+	int senderID;
+}
+
+struct heartBeat
+{
+	int senderID;
+	int currentState; //change to state_t when implemented
+	int currentFloor;
+}
+
+
+
+
+messageThread()
+{
+	if delegateOrder
+	{
+		send(keeperOfSets, delegateOrder);
+		debug
+		{
+		writeln(delegateOrder);
+		}
+	}
+	
+	else if confirmOrder
+	{
+		send(keeperOfSets, confirmOder);
+		debug
+		{
+		
+		}
+	}
+	
+	
+	
+	
+	
+}
