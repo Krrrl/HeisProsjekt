8e2f7572cf8317cfc23c6e242dd17befcaadb1f0
Magne Hov <magne.hov@gmail.com>
2017-03-02 17:17:18 +0100

    removed binary file

--- src/operator.d
+++ src/operator.d
@@ -39,46 +39,12 @@ void operatorThread(
 {
 	debug writeln("    [x] operatorThread");
 
-	shared static message_t testOrder = {
-		type : message_header_t.delegateOrder,
-		senderID : "1",
-		targetID : "2",
-        orderFloor : 2,
-        orderDir : direction_t.DOWN,
-		currentState : state_t.GOING_UP,
-		currentFloor : 1,
-		timestamp : 0
-	};
 
-    Thread.sleep(seconds(1));
 
-	// Construct prevState for all buttons
-	bool[main.nrOfFloors][3] buttonPrevMatrix = false;
 
 	while (true)
 	{
-        writeln("true?");
 		// Check panels
-		foreach (floor; 0..main.nrOfFloors)
-		{
-            writeln(floor);
-			foreach (buttonType; elevButtonTypes())
-			{
-                bool buttonState = cast(bool)(elev_get_button_signal(buttonType, floor));
-                bool prevButtonState = buttonPrevMatrix[cast(int)(buttonType)][floor];
-                debug writeln(buttonState);
-
-				if (buttonState && !prevButtonState)
-                {
-					debug writeln("buttontype:", buttonType, "pressed on floor ", floor);
-
-                    buttonPrevMatrix[cast(int)(buttonType)][floor] = true;
-                }
-                else if (!buttonState && prevButtonState)
-                {
-                    buttonPrevMatrix[cast(int)(buttonType)][floor] = false;
-                }
-			}
 		}
 
 
